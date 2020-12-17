6c43355ba439103d0f2bcd5b2cb2acf30a4c9a56
poornimavithanage <vithanagepurnima@gmail.com>
2020-12-17 17:09:46 +0530

    Modify ReverseOrder File

--- ReverseOrder/src/com/virtusa/ReverseOrder.java
+++ ReverseOrder/src/com/virtusa/ReverseOrder.java
@@ -1,16 +1,31 @@
 package com.virtusa;
 
+import java.math.BigInteger;
+import java.util.Scanner;
+
 public class ReverseOrder {
+ public static void reverse(BigInteger number){
+
+      BigInteger reverseNumber = new BigInteger(String.valueOf(number));
+      BigInteger calculationNumber = new BigInteger("10");
+
+      if(reverseNumber.compareTo(calculationNumber)==-1){
+          System.out.println(reverseNumber);
+          return;
+      }else {
+          System.out.print(reverseNumber.mod(calculationNumber));
+          reverseNumber = reverseNumber.divide(calculationNumber);
+          reverse(reverseNumber);
+      }
+  }
+
     public static void main(String[] args) {
-        int num =819184122, reversed = 0;
+        System.out.println("Enter Number to be reversed");
+        Scanner scanner = new Scanner(System.in);
 
-        while(num != 0) {
-            int digit = num % 10;
-            reversed = reversed * 10 + digit;
-            num /= 10;
-        }
-
-        System.out.println("Reversed Number: " + reversed);
+        BigInteger number = scanner.nextBigInteger();
+        System.out.println("The reversed Number is " );
+        reverse(number);
 
     }
 }
