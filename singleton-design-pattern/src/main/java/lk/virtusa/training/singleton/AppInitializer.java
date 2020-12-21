b3b4cfd03f319224147d19968c45044b4c89c6fb
poornimavithanage <vithanagepurnima@gmail.com>
2020-12-21 15:28:33 +0530

    Add singleton design pattern file

--- singleton-design-pattern/src/main/java/lk/virtusa/training/singleton/AppInitializer.java
+++ singleton-design-pattern/src/main/java/lk/virtusa/training/singleton/AppInitializer.java
@@ -1,2 +1,36 @@
-package lk.virtusa.training.singleton;public class AppInitializer {
+package lk.virtusa.training.singleton;
+
+import java.sql.Connection;
+
+public class AppInitializer {
+    public static void main(String[] args) {
+
+        long start;
+        long end;
+
+        Employee employee = Employee.getEmployee(); //1st instance
+        System.out.println(employee);
+
+        start=System.currentTimeMillis();
+        try {
+            Connection connection = employee.getConnection(); //create a connection variable
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        end = System.currentTimeMillis();
+        System.out.println(end-start);
+
+        Employee employee1 = Employee.getEmployee(); //2nd instance
+
+        start=System.currentTimeMillis();
+
+        try {
+            Connection connection1 = employee1.getConnection();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        end = System.currentTimeMillis();
+
+        System.out.println(end-start);
+    }
 }
