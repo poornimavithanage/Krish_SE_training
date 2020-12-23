f57f0ca496a84341dce654815b5a7a2be63efbf0
poornimavithanage <vithanagepurnima@gmail.com>
2020-12-23 13:07:17 +0530

    Store the amount to dispense and used by the chain implementations

--- DesignPattern/chain_of_responsibility-design-pattern/src/lk/virtusa/training/chainofresponsibility/Currency.java
+++ DesignPattern/chain_of_responsibility-design-pattern/src/lk/virtusa/training/chainofresponsibility/Currency.java
@@ -1,5 +1,14 @@
 package lk.virtusa.training.chainofresponsibility;
 
 public class Currency {
-    
+    /*store the amount to dispense handler */
+    private int amount;
+
+    public Currency(int amount) {
+        this.amount = amount;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
 }
