2ec905848d8602d3e25a3f2fd6c3bdef5de2a67a
poornimavithanage <vithanagepurnima@gmail.com>
2020-12-24 16:20:58 +0530

    Add CareTaker.java

---
+++ DesignPattern/memento-design-pattern/src/lk/virtusa/training/memento/CareTaker.java
@@ -0,0 +1,18 @@
+package lk.virtusa.training.memento;
+
+import java.util.Stack;
+
+public class CareTaker {
+    Stack<PlaceOrder.PlaceOrderMemento>history = new Stack<>();
+
+    public void save(PlaceOrder placeOrder){
+        history.push(placeOrder.save());
+    }
+
+    public void revert(PlaceOrder placeOrder){
+        if(!history.empty())
+            placeOrder.revert(history.pop());
+        else
+            System.out.println("CAN NOT UNDO");
+    }
+}
