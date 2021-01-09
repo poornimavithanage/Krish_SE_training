cac2248450b60ee7dcb8dafe0ec4c891e872f237
poornimavithanage <vithanagepurnima@gmail.com>
2021-01-09 13:08:27 +0530

    create model common

--- Assignment[2]/common-model/src/main/java/com/poornima/commonmodel/project/Project.java
+++ Assignment[2]/common-model/src/main/java/com/poornima/commonmodel/project/Project.java
@@ -1,2 +1,18 @@
-package com.poornima.commonmodel.project;public class Project {
+package com.poornima.commonmodel.project;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+@Entity
+@Table(name = "project")
+@Data
+public class Project {
+
+@Id
+    private int id;
+    private String projectName;
+    private String status;
+
 }
--- Assignment[2]/common-model/src/main/java/com/poornima/commonmodel/task/Task.java
+++ Assignment[2]/common-model/src/main/java/com/poornima/commonmodel/task/Task.java
@@ -1,2 +1,21 @@
-package com.poornima.commonmodel.task;public class Task {
+package com.poornima.commonmodel.task;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "task")
+@Data
+public class Task {
+
+    @Id
+    private String taskId;
+    private int projectId;
+    private String projectName;
+    private String taskName;
+    private String assignedId;
+    private String assignedName;
 }
